fb9f756945fe0e59abfc687e5d394a2bdb7b5a0f
park yongseong <3358695@gmail.com>
2022-06-27 15:13:29 +0900

    sql 로그설정중..

--- bi-solution/src/main/java/com/teraenergy/bisolution/realEstate/RealEstateController.java
+++ bi-solution/src/main/java/com/teraenergy/bisolution/realEstate/RealEstateController.java
@@ -1,37 +1,34 @@
 package com.teraenergy.bisolution.realEstate;
 
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 
-import org.json.JSONArray;
-import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.teraenergy.global.service.CommonService;
 
-import groovy.util.logging.Slf4j;
-
-@Slf4j
 @RestController
-@RequestMapping("/api/realEstate")
+@RequestMapping("/api")
 public class RealEstateController {
 
     @Resource(name="commonService")
     private CommonService commonService;
     
     private static final String PROGRAM_ID = "RealEstate";
+    
+    @GetMapping("/all-trade-real-apt")
+    public Object getAllTradeRealApt(RealEstateDTO realEstateDTO) throws Exception {
 
-    @GetMapping("/tradeRealAptList")
-    public Object tradeRealAptList(RealEstateDTO realEstateDTO) throws Exception {
-
-        List<RealEstateDTO> data = (List<RealEstateDTO>) commonService.selectList(realEstateDTO, PROGRAM_ID + ".getTradeTealAptList");
-        System.out.println(data);
         Map<String,Object> result = new HashMap<>();
+        
+        List<RealEstateDTO> data = (List<RealEstateDTO>) commonService.selectList(realEstateDTO, PROGRAM_ID + ".getTradeRealAptList");
+
 		result.put("data", data);
         result.put("sucess", true);
 
--- bi-solution/src/main/java/com/teraenergy/bisolution/realEstate/RealEstateDTO.java
+++ bi-solution/src/main/java/com/teraenergy/bisolution/realEstate/RealEstateDTO.java
@@ -2,18 +2,19 @@ package com.teraenergy.bisolution.realEstate;
 
 import org.apache.ibatis.type.Alias;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Alias("realEstateDTO")
 @Getter
 @Setter
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
 public class RealEstateDTO {
 
-    private int idx;
+    private Integer idx;
     private String aptNm;
     private String layer;
     private String payment;
@@ -27,26 +28,4 @@ public class RealEstateDTO {
     private String landNo;
     private String landArea;
 
-    // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-
-
-    @Override
-    public String toString() {
-        return "{" +
-            " idx='" + idx + "'" +
-            ", aptNm='" + aptNm + "'" +
-            ", layer='" + layer + "'" +
-            ", payment='" + payment + "'" +
-            ", builtYear='" + builtYear + "'" +
-            ", tradeYear='" + tradeYear + "'" +
-            ", tradeMonth='" + tradeMonth + "'" +
-            ", sigunguCd='" + sigunguCd + "'" +
-            ", eupmyundongCd='" + eupmyundongCd + "'" +
-            ", dong='" + dong + "'" +
-            ", roadNm='" + roadNm + "'" +
-            ", landNo='" + landNo + "'" +
-            ", landArea='" + landArea + "'" +
-            "}";
-    }
-    
 }
