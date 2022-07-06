6cdd01232065e30d356210f54d20ae21d0809ebe
pinkspidar <pinkspidar@teraenergy.co.kr>
2022-07-06 17:11:19 +0900

    테스트

---
+++ src/main/java/com/teraenergy/bisolution/lifeSatisfaction/LifeSatisfaction.kt
@@ -0,0 +1,10 @@
+package com.teraenergy.bisolution.lifeSatisfaction
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/lifeSatisfaction")
+class LifeSatisfaction {
+
+}
