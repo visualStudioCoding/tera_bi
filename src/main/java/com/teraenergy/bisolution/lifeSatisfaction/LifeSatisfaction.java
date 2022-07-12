d48e1b1c8318e214b674f6e6e74a0cd3353cc1a1
pinkspidar <pinkspidar@teraenergy.co.kr>
2022-07-12 17:12:22 +0900

    삶의만족도

--- src/main/java/com/teraenergy/bisolution/lifeSatisfaction/LifeSatisfaction.java
+++ src/main/java/com/teraenergy/bisolution/lifeSatisfaction/LifeSatisfaction.java
@@ -36,7 +36,7 @@ public class LifeSatisfaction {
     @Resource(name = "commonService")
     private CommonService commonService;
 
-    @GetMapping("/life/main")
+    @GetMapping("/lifeSatisfaction/main")
 //    public String mainIndex(HttpServletResponse response,Model model) throws IOException {
     public String mainIndex(Model model) throws IOException {
         log.info(DIRECTORY + PROGRAM_ID + "Main");
@@ -55,9 +55,9 @@ public class LifeSatisfaction {
     }
 
     @ResponseBody
-    @GetMapping("/life/LifeSatisfaction")
+    @GetMapping("/lifeSatisfaction/LifeSatisfaction")
     public Object LifeSatisfaction_Parse(String url, String parameter) throws Exception {
-        log.info(DIRECTORY + PROGRAM_ID + "Main");
+        log.info(DIRECTORY + PROGRAM_ID + "LifeSatisfaction");
         //return DIRECTORY + PROGRAM_ID + "Main";
         System.out.println("url : " + url);
         System.out.println("parameter : " + parameter);
@@ -166,9 +166,9 @@ public class LifeSatisfaction {
 
     @Transactional(rollbackFor = Exception.class)
     @ResponseBody
-    @GetMapping("/life/Marriage")
+    @GetMapping("/lifeSatisfaction/Marriage")
     public Object Marriage_Parse(String url, String parameter) throws Exception {
-        log.info(DIRECTORY + PROGRAM_ID + "Main");
+        log.info(DIRECTORY + PROGRAM_ID + "Marriage");
         //return DIRECTORY + PROGRAM_ID + "Main";
         System.out.println("url : " + url);
         System.out.println("parameter : " + parameter);
@@ -278,4 +278,121 @@ public class LifeSatisfaction {
 
         return result;
     }
+
+    @Transactional(rollbackFor = Exception.class)
+    @ResponseBody
+    @GetMapping("/lifeSatisfaction/Divorce")
+    public Object Divorce_Parse(String url, String parameter) throws Exception {
+        log.info(DIRECTORY + PROGRAM_ID + "Divorce");
+        System.out.println("url : " + url);
+        System.out.println("parameter : " + parameter);
+
+        //kosis = json, enara = xml
+        String format = "json";
+        String site = "kosis";
+        StringBuilder stringBuilder = commonService.getApiResult(url, parameter, format, site);
+        JSONArray jsonList = (JSONArray) commonService.apiJsonParser(stringBuilder);
+        //
+        // {"err":"30","errMsg":"데이터가 존재하지 않습니다."}
+
+        Map<String, Object> result = new HashMap<>();
+        Map<String, Object> dataMap = new HashMap<>();
+        int cnt=1;
+        for (Object jsonObject : jsonList) {
+            JSONObject jsonData = (JSONObject) jsonObject;
+
+            //년도
+            String prdde = (String) jsonData.get("PRD_DE");
+            String yrdt = prdde.substring(0, 4);
+
+            //월
+            String monDt = prdde.substring(4, 6);
+
+            //시도코드
+            String ctyCode = (String) jsonData.get("C1");
+
+            //시도
+            String ctyNm = "";
+
+            //구는 뺀다
+            if(ctyCode.length() >= 5 && !"0".equals(ctyCode.substring(4,5))) {
+                continue;
+            }
+
+            //청주 ? 중복됨
+            if("33040".equals(ctyCode)) {
+                continue;
+            }
+
+            if(ctyCode.equals("00")) {
+                ctyNm = "전국";
+            } else if("11".equals(ctyCode.substring(0,2))) {
+                ctyNm = "서울특별시";
+            } else if("21".equals(ctyCode.substring(0,2))) {
+                ctyNm = "부산광역시";
+            } else if("22".equals(ctyCode.substring(0,2))) {
+                ctyNm = "대구광역시";
+            } else if("23".equals(ctyCode.substring(0,2))) {
+                ctyNm = "인천광역시";
+            } else if("24".equals(ctyCode.substring(0,2))) {
+                ctyNm = "광주광역시";
+            } else if("25".equals(ctyCode.substring(0,2))) {
+                ctyNm = "대전광역시";
+            } else if("26".equals(ctyCode.substring(0,2))) {
+                ctyNm = "울산광역시";
+            } else if("29".equals(ctyCode.substring(0,2))) {
+                ctyNm = "세종특별자치시";
+            } else if("31".equals(ctyCode.substring(0,2))) {
+                ctyNm = "경기도";
+            } else if("32".equals(ctyCode.substring(0,2))) {
+                ctyNm = "강원도";
+            } else if("33".equals(ctyCode.substring(0,2))) {
+                ctyNm = "충청북도";
+            } else if("34".equals(ctyCode.substring(0,2))) {
+                ctyNm = "충청남도";
+            } else if("35".equals(ctyCode.substring(0,2))) {
+                ctyNm = "전라북도";
+            } else if("36".equals(ctyCode.substring(0,2))) {
+                ctyNm = "전라남도";
+            } else if("37".equals(ctyCode.substring(0,2))) {
+                ctyNm = "경상북도";
+            } else if("38".equals(ctyCode.substring(0,2))) {
+                ctyNm = "경상남도";
+            } else if("39".equals(ctyCode.substring(0,2))) {
+                ctyNm = "제주";
+            } else if("90".equals(ctyCode.substring(0,2))) {
+                ctyNm = "국외";
+            }
+
+            //시도 , 시군구
+            String dstNm = (String) jsonData.get("C1_NM");
+
+            //점수단위(건)
+            String unit = (String) jsonData.get("UNIT_NM");
+
+            //점수
+            String val = (String)jsonData.get("DT");
+
+            dataMap.put("yrdt", yrdt);
+            dataMap.put("mondt", monDt);
+            dataMap.put("areacd", ctyCode);
+            dataMap.put("ctynm", ctyNm);
+            dataMap.put("dstnm", dstNm);
+            dataMap.put("unit", unit);
+            dataMap.put("val", val);
+
+
+
+            commonService.insertContents(dataMap, PROGRAM_ID + ".insertDivorce");//LifeSatisfaction.insertMarriage
+            cnt++;
+        }
+
+        result.put("data", dataMap);
+        result.put("size", cnt);
+        result.put("success", "성공");
+
+        return result;
+    }
+
+
 }
